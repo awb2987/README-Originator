e94049408f6458ba0f20ca49df41a1b877046673
Alexander Braff <awb2987@gmail.com>
2024-09-29 18:54:49 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,71 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown'); // Import the generateMarkdown function
+
+// Create an array of questions for the user to supply
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please provide a description of your project:',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Please provide installation instructions for your project:',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Please provide your project usage information:',
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'Please provide your project contributing guidelines:',
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Please provide your project test instructions:',
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Please choose a license for your project:',
+        choices: ['MIT', 'GPL', 'Apache', 'BSD'],
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Please enter your GitHub username:',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Please enter your email address:',
+    },
+];
+
+// Function to write the README file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) =>
+        err ? console.log(err) : console.log('README.md has been created!')
+    );
+}
+
+// Function to initialize the app
+function init() {
+    inquirer.prompt(questions).then((answers) => {
+        const readmeContent = generateMarkdown(answers); // Use the generateMarkdown function
+        writeToFile('README.md', readmeContent);
+    });
+}
+
+// Function call to initialize the app
+init();
---
+++ utils/generateMarkdown.js
@@ -0,0 +1,64 @@
+// utils/generateMarkdown.js
+
+// Function to return a license badge based on the desired license type
+function renderLicenseBadge(license) {
+  if (license === 'None') return ''; // No badge because no license chosen
+  return `![License](https://img.shields.io/badge/license-${license.replace(' ', '%20')}-blue.svg)`;
+}
+
+// Function to return the license link in the README
+function renderLicenseLink(license) {
+  if (license === 'None') return ''; // No link because no license chosen
+  const licenseLinks = {
+    'MIT': 'https://opensource.org/licenses/MIT',
+    'GPL': 'https://opensource.org/licenses/GPL-3.0',
+    'Apache': 'https://opensource.org/licenses/Apache-2.0',
+    'BSD': 'https://opensource.org/licenses/BSD-3-Clause'
+  };
+  return licenseLinks[license];
+}
+
+// Function to return the license section of the README
+function renderLicenseSection(license) {
+  if (license === 'None') return ''; // No license section because no license chosen
+  return `## License
+This project is licensed under the [${license}](${renderLicenseLink(license)}) License.`;
+}
+
+// Function to generate markdown for the README
+function generateMarkdown(data) {
+  return `# ${data.title}
+
+${renderLicenseBadge(data.license)}
+
+## Description
+${data.description}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+${renderLicenseSection(data.license)}
+
+## Contributing
+${data.contributing}
+
+## Tests
+${data.tests}
+
+## Questions
+If you have any questions about this app, please contact me on [GitHub](https://github.com/${data.github}) or email me at ${data.email}.
+`;
+}
+
+module.exports = generateMarkdown;
